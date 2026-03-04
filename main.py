018f3c15b0333a9a8b12553398dc36b22246efeb
Artem Savchenko <256005153+Dvoix@users.noreply.github.com>
2026-03-04 19:13:45 +0000

    Bug: bot not working

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import asyncio
 from core.bot import bot, dp
-from handlers import start, help
+from handlers import start, help, spotify
 
 async def main():
-    print("Bot started!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
